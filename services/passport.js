5468b5a499a614f8c6b575f210c928de44d8f72e
shayZohar <shayzohar8@gmail.com>
2020-06-30 15:13:25 +0300

    add proxy:true

--- services/passport.js
+++ services/passport.js
@@ -27,6 +27,7 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		// CALLBACK FUNCTION - the code of info that comes back from google after log in and come back to callback route
 		// the calls for mongo are all asynchronous, so it uses PROMISE (we will refactor it later to better method)
