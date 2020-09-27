002da3d1a3e645505a2d0eebb303ee3e9d7fce6c
shayZohar <shayzohar8@gmail.com>
2020-09-27 19:52:03 +0300

    fix keys shown

--- config/keys.js
+++ config/keys.js
@@ -5,9 +5,3 @@ if (process.env.NODE_ENV === 'production') {
     // we are in development mode
     module.exports = require('./dev');
 }
-
-// mongodb+srv://ShayZoharProd:Simtatyona2020@cluster0.uu2jq.mongodb.net/emaily-prod?retryWrites=true&w=majority
-
-
-//prod-clientId: 762225303819-mjtm9ti1hmlt8o7tk4hpv8srulbh55ne.apps.googleusercontent.com
-// prod-secret: vJHL5bSiIGRPyzTvX5FUGH-3
